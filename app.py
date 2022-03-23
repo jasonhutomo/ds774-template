b7e758a8356b5444fec5d4b813bb679ffa7bd83d
Jason Hutomo~ <jason.hutomo@unh.ede>
2022-03-22 20:07:10 -0400

    l

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ def contact():
     else:
         return render_template('contact.html', message=message)
 
-@app.route("/issueslog", methods=['GET', 'POST'])
-def issueslog():
+@app.route("/admin", methods=['GET', 'POST'])
+def admin():
     error = ''
     records = ''
     print(request)
@@ -54,7 +54,7 @@ def issueslog():
     if request.method == 'POST':
 
         # If the form was Login, perform log in steps
-        if request.form.get('issueslog') == 'Login':
+        if request.form.get('admin') == 'Login':
             username = request.form['username']
             password = request.form['password']
 
@@ -72,7 +72,7 @@ def issueslog():
                 error = 'Invalid Username or Password'
         
         # if form was logout button, end user session
-        elif request.form.get('issueslog')  == 'Logout':
+        elif request.form.get('admin')  == 'Logout':
             session.pop('user_id')
 
         
@@ -81,7 +81,7 @@ def issueslog():
         records = get_records()
 
     # return the admin page, showing any message or data that we may have
-    return render_template('issueslog.html', error = error, records = records)
+    return render_template('admin.html', error = error, records = records)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -97,7 +97,7 @@ def register():
         if get_user(username):
             new_id = add_user(username, password)
             error = "Registration sucessful. Please login"
-            return render_template('issueslog.html', error = error)
+            return render_template('admin.html', error = error)
         else:
             error = f"Username {username} not available"
         
@@ -116,14 +116,14 @@ def edit():
             message = request.form['message']
             print(fname, lname, eaddress, message)
             edit_record(msg_id, fname, lname, eaddress, message)
-            return redirect('/issueslog')
+            return redirect('/admin')
 
         elif request.form.get('edit') == 'cancel':
-            return redirect('/issueslog')
+            return redirect('/admin')
         
-        elif request.form.get('issueslog') == 'Delete':
+        elif request.form.get('admin') == 'Delete':
             delete_record(msg_id)
-            return redirect('/issueslog')
+            return redirect('/admin')
 
 
     entry = get_single_record(msg_id)
